b03b26bc4e3083aca549482c141b96ecf72ff3b9
Axel Scheele <axel@Axels-MacBook-Pro.local>
2016-10-31 17:40:07 +0100

    added pages

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express')
 
 var  bodyParser = require('body-parser')
 
+var WebSocketServer = require('ws').Server
+
+var wss = new WebSocketServer({port:5555})
+
+
+
+
+
+
+
 var app = express()
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -13,7 +23,7 @@ var orders = [];
 app.post('/neworder', function(req, res){
 
     var body = req.body;
-
+    console.log('BODY',body)
     orders.push(body)
     res.sendStatus(200)
 })
@@ -24,6 +34,16 @@ app.get('/takeorder', function(req, res){
     res.json(200, orders.shift())
 })
 
+app.get('/stack', function(req, res){
+    res.json(200, orders)
+})
+
+app.get('/stackview',function(req, res){
+    res.sendFile(__dirname +'/web/stack.html')
+
+})
+
+app.use(express.static('web'))
 
 if (process.env.mode === 'TEST')
     module.exports = {app:app, stack:orders};
